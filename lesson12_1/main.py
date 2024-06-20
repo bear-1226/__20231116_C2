c2a69110b7d63e11886e41ed2d359ed5518a623b
Bear1226 <bear861226@gmail.com>
2024-06-20 11:37:01 +0800

    123

--- lesson12_1/main.py
+++ lesson12_1/main.py
@@ -8,8 +8,3 @@ if __name__ == "__main__":
     fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
     save_to_csv(students,fileName)
 
-if __name__ == "__main__":
-    s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
-    students:list[dict]= getStudents(s_nums)
-    fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名):")
-save_to_csv(students,fileName)
